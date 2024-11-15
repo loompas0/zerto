499957ad8d81cc3bc87b9cf555b12f51ae6f2d70
Remy Bernheim <remy.bernheim@hpe.com>
2024-11-15 17:05:51 +0100

    Comment VPGmofication added

--- VpgModificationLtr7Years.ps1
+++ VpgModificationLtr7Years.ps1
@@ -1,15 +1,63 @@
+<#
+Legal Disclaimer
+This script is an example script and is not supported by any means.
+The author further disclaim all implied warranties including, 
+without limitation, any implied warranties of merchantability 
+or of fitness for a particular purpose.
+In no event shall  its authors or anyone else involved in 
+the creation, production or delivery of the scripts be liable for 
+any damages whatsoever 
+(including, without limitation, damages for loss of business profits, business interruption, 
+loss of business information, or other pecuniary loss) arising out of the use of or the inability 
+to use the sample scripts or documentation, 
+even if the author  has been advised of the possibility of such damages. 
+The entire risk arising out of the use or performance of the sample scripts 
+and documentation remains with you.
+#>
+
+<#
+The Author is Loompas0 who is a pseudo.
+The source site of the code couldbe https://github.com/loompas0/ 
+if you are explicitely or implicitely authorized to access to it.
+#>
+
+<#This script has been tested on a Windows environment With Zerto 10.5 version #>
+
+<#
+*** Title :             VpgModificationLtr7Years ***
+*** Aim :               Test a way to modify some settings in a VPG 
+                        Here the Yearly retention period ***
+*** Input Parameters :  None ***
+*** Output Parameters : None ***
+*** Files needed :
+        --- connect-secret.ps1 --- Program to connect to the ZVM
+        --- datacenters.json --- File with DC credentials  ***
+#>
+
+
+# History 
+# - 15/11/2023 creation and first tests
+# - 
+# =====================================================================================
+
 # Vpg modification ltr 7 Years 
 
-# first get credential and token to be used 
-# use json file to connect to all sites
+############################################################
+# First get Credential and token to be used for connection #
+############################################################ 
+
+# use json file (datacenters.json) to connect to all sites 
 . ./connect-secret.ps1
-# Now that we have a token because of successful Keycloak authentication, we can proceed with Zerto REST API calls 
+# Now that we have a token because of successful Keycloak authentication, we can proceed with Zerto REST API calls
+# Now we have 
+#       * The basic address of the zvm          : $zvmApiBase
+#       * The token to be used as a Bearer      : $token
 
 #########################################
 # Second Retreive VPG identifier        #
-#########################################
+######################################### 
 
-
+#API Call
 $request = @{
 
     Headers     = @{
@@ -32,12 +80,13 @@ catch {
     Write-Error "Error making API call to ZVM" -ErrorAction Stop
 }
 
-# write-output $result
+# write  $result
 $result | Select-Object VpgIdentifier, VPGName,VMsCount, SourceSite, TargetSite, ActualRPO, IOPS, ThroughputInMB | format-table 
-# $VpgName = $result.VpgName
-# $VpgId = $result.VpgIdentifier
 
-# lets print VPG To choose the right one 
+
+# lets print all VPG To choose the right one if there is more than one 
+
+# Function used in a loop to list all VPG
 function Show-VPG-List
 {
     
@@ -46,6 +95,8 @@ function Show-VPG-List
     Write-Host $textline -ForegroundColor Green
 
 }
+
+# Select VPG depending of the answer from the screen
 
 if ($result.Count -eq 1) 
 {
@@ -57,12 +108,9 @@ else
     Write-Host “== $VpgTitle ==”
     for (($i=0); $i -lt $result.count; $i++)
     {
-        
         $VpgName = $result.VpgName[$i]
         $VpgId = $result.VpgIdentifier[$i]
-        
         Show-VPG-List
-        
     }
     $response = Read-Host “Please make a selection”
     if ($response -gt $result.count-1) 
@@ -72,17 +120,16 @@ else
     $VpgIdentifier = $result.VpgIdentifier[$response]
 }
 
-#VpgIndentifier is in variable $VpgIdentifier we will use it later on 
 # Now we have 
+#       * The basic address of the zvm              : $zvmApiBase
+#       * The token to be used as a Bearer          : $token
 #       * the id of the VPG to be modified          : $VpgIdentifier
 
-#########################################
-# Third Lets create a VPG Setting         #
-#########################################
+############################################################
+# Third Lets create a VPG Setting from semected VPG        #
+############################################################
 
 # API CALL
-
-
 $Headers = @{
     "Content-Type"  = "application/json"
     Authorization = "Bearer ${token}"
@@ -91,44 +138,36 @@ $Headers = @{
 $Body = "{`"vpgIdentifier`" :  `"$VpgIdentifier`"}"
 $UriApi = $zvmApiBase + "vpgSettings/"
 
-
 $result = Invoke-RestMethod -Uri $UriApi  -Headers $Headers -Method Post -Body $Body -SkipCertificateCheck
-
-
-
 $VpgSettingsIdentifier = $result
 
-
-# VpgSettingsIndentifier is in variable $VpgSettingsIdentifier we will use it later on to manipulate
 # Now we have 
+#       * The basic address of the zvm              : $zvmApiBase
+#       * The token to be used as a Bearer          : $token
 #       * the id of the VPG to be modified          : $VpgIdentifier
 #       * the id of the modifications (settings)    : $VpgSettingsIdentifier
-
 
 #########################################
 # Fourth Lets retrieve all VPG Setting  #
 #########################################
 
+# API CALL
 $Headers = @{
     "Content-Type"  = "application/json"
     Authorization = "Bearer ${token}"
     Accept = "application/json"
 }
-
 $UriApi = $zvmApiBase + "vpgSettings/" + $VpgSettingsIdentifier
 
-
 $result = Invoke-RestMethod -Uri $UriApi  -Headers $Headers -Method Get  -SkipCertificateCheck
-
 $VpgSettings = $result
 
-
-# all VpgSettings information is in variable $VpgSettings we will use it later on to manipulate
 # Now we have 
+#       * The basic address of the zvm              : $zvmApiBase
+#       * The token to be used as a Bearer          : $token
 #       * the id of the VPG to be modified          : $VpgIdentifier
 #       * the id of the modifications (settings)    : $VpgSettingsIdentifier
 #       * the full settings of the VPG              : $VpgSettings 
-
 
 ##############################################
 # Fith Enter nb year and Change VpgSettings  #
@@ -136,8 +175,6 @@ $VpgSettings = $result
 
 #  Read the number of years and put it in Nb Years
 #  to be used in  $VpgSettings.LongTermRetention.SchedulerPolicy.Yearly.RetentionDuration    
-# 
-
 $NbYearsString = Read-Host "Please enter Number of years : (should be less than 8)"
 $NbYears = [int]$NbYearsString
 
@@ -146,27 +183,25 @@ if ($NbYears -gt 8)
     Write-Error "Number of year is out of scope" -ErrorAction Stop
 }
 
+# Change the Number of years for LTR retention duration
 $VpgSettings.LongTermRetention.SchedulerPolicy.Yearly.RetentionDuration.Count = $NbYears
 
-# put the variable $VpgSettings to Json format
-
+# Convert the variable $VpgSettings to Json format $VpgSettingsJson
 $VpgSettingsJson = $VpgSettings | ConvertTo-Json -Depth 7
-# $VpgSettingsJson = "'" + $VpgSettingsJson + "'"
-
 
 # Now we have 
+#       * The basic address of the zvm              : $zvmApiBase
+#       * The token to be used as a Bearer          : $token
 #       * the id of the VPG to be modified          : $VpgIdentifier
 #       * the id of the modifications (settings)    : $VpgSettingsIdentifier
 #       * the full settings of the VPG              : $VpgSettings modified with correct number of years
-#       * The full settings of VPG in Json format   : $VpgSettingsJson
-
+#       * The full settings of VPG in Json format   : $VpgSettingsJson modified with correct nb of year
 
 ##############################################
-# Sixth  Modify Vpg settings                  #
+# Sixth  Modify Vpg settings in ZVM          #
 ##############################################
 
-# Api call
-
+# API CALL
 $Headers = @{
     "Content-Type"  = "application/json"
     Authorization = "Bearer ${token}"
@@ -175,23 +210,22 @@ $Headers = @{
 $Body = $VpgSettingsJson 
 $UriApi = $zvmApiBase + "vpgSettings/" + $VpgSettingsIdentifier
 
-
 $result = Invoke-RestMethod -Uri $UriApi  -Headers $Headers  -Body $Body -Method Put  -SkipCertificateCheck
 
 # Now we have 
+#       * The basic address of the zvm              : $zvmApiBase
+#       * The token to be used as a Bearer          : $token
 #       * the id of the VPG to be modified          : $VpgIdentifier
 #       * the id of the modifications (settings)    : $VpgSettingsIdentifier
 #       * the full settings of the VPG              : $VpgSettings modified with correct number of years
-#       * The full settings of VPG in Json format   : $VpgSettingsJson
-
+#       * The full settings of VPG in Json format   : $VpgSettingsJson modified with correct nb of year
+#       * The seetings modified in ZVM
 
 ##############################################
 # Seventh  Commit Vpg settings to modify VPG #
 ##############################################
 
 # API CALL
-
-
 $Headers = @{
     "Content-Type"  = "application/json"
     Authorization = "Bearer ${token}"
@@ -203,4 +237,6 @@ $UriApi = $zvmApiBase + "vpgSettings/" + $VpgSettingsIdentifier + "/commit"
 
 $result = Invoke-RestMethod -Uri $UriApi  -Headers $Headers  -Method Post  -SkipCertificateCheck
 
-Write-Host $result
+Write-Host "The VPG  $VpgSettingsIdentifier has $NBYears Years of retention"
+
+# *** End of Program ***
